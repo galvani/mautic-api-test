d6b6fa0aa87e134abd281f77c7bc59e548b9b000
Jan Kozak <galvani78@gmail.com>
2023-12-20 22:36:12 +0100

    restrict psr log

--- test-batch-create.php
+++ test-batch-create.php
@@ -13,8 +13,9 @@ $settings = [
     'version'      => 'OAuth2', // Version of the OAuth can be OAuth2 or OAuth1a. OAuth2 is the default value.
     'clientKey'    => '2_3pzekjxdhtuswwkcoow4wo48os04wow8sgo4k4og84ggcwo80k',       // Client/Consumer key from Mautic
     'clientSecret' => '5d6z47ps7xssgg0w08csokskskgs4oswc8s408w8gkgko04kk4',       // Client/Consumer secret key from Mautic
-    'accessToken'  => 'N2E5ZTlmMjdkM2FjOGVjMDJjYTFjMTYxMzcyYmIwOTJhOTMxNjgxYjJhOTE3YjBmM2VmZWVkZjA3MDc4NTY4MA',       // Client/Consumer key from Mautic
+    'accessToken'  => 'ZjQ2MTJhNzJjZmU1ZWE5NmZkOTA4MjQ2Mzg5MTZlNjg3ZWRmMTI0YjFhNWE3OTJmZjkyMTA5ZmZkNmViNDE1Nw',
 ];
+
 
 // Initiate the auth object specifying to use BasicAuth
 $initAuth = new ApiAuth();
@@ -30,6 +31,33 @@ $api        = new \Mautic\MauticApi();
 $contactApi = $api->newApi('contacts', $auth, $settings['baseUrl'].'/api');
 $contactApi->setLogger($logger);
 echo "Creating contact...\n";
+
+$batch = [
+    [ 
+        "email" => "jean.c03t@gmail.com", 
+        "customObjects" => [
+            "data" => [
+                [
+                    "id"   => 1,
+                    "data" => [
+                        ["name"       => "Hello From API2",
+                        "attributes" => [
+                            "updated" => "2018-10-10T14:00:00+02:00"
+                        ]
+
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
+
+$response = $contactApi->createBatch($batch, ['includeCustomObjects' => true],);
+print_r($response);
+die();
+
+
 
 $response = $contactApi->createBatch(
     [
